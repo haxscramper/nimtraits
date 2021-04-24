6c7e0502832fc1aa5b26ad10ec5502debfefdef7
haxscramper <haxscramper@gmail.com>
2021-04-25 00:00:52 +0300

    [FEATURE] `mapCase` reimplementation

--- src/nimtraits/trait_xml.nim
+++ src/nimtraits/trait_xml.nim
@@ -4,6 +4,7 @@ import hmisc/helpers
 
 import hpprint
 
+proc newIdent*(str: string): NimNode = newIdentNode(str)
 proc dotField*[N](self: N, field: ObjectField[N]): N =
   newNTree[N](nnkDotExpr, self, newNIdent[N](field.name))
 
@@ -20,24 +21,27 @@ proc newObjectPathElem*[N](
       isElse: false, kindField: field,
       ofValue: normalizeSet(branch.ofValue))
 
+type
+  KindFields[N] = seq[ObjectField[N]] # Using this typedef makes semcheck fail. 10/10
+  NKindFields = KindFields[NimNode]
 
 proc mapBranches*(
-    field: NObjectField, self: NimNode, parent: NObjectPath,
-    caseExpr: proc(path: NObjectPath): NimNode,
-    mapBranch: proc(path: NObjectPath, branch: NObjectBranch): NimNode
+    field: NObjectField, parent: seq[ObjectField[NimNode]],
+    caseExpr: proc(path: seq[ObjectField[NimNode]]): NimNode,
+    mapBranch: proc(path: seq[ObjectField[NimNode]], branch: NObjectBranch): NimNode
   ): NimNode =
 
   if field.isKind:
     result = nnkCaseStmt.newTree(caseExpr(parent))
     for branch in field.branches:
-      let thisPath = parent & newObjectPathElem(field, branch)
+      let thisPath = parent & field # newObjectPathElem(field, branch)
       let cbRes = mapBranch(thisPath, branch).nilToDiscard()
 
       var branchBody = newStmtList()
       for field in branch.flds:
         if field.isKind:
           branchBody.add field.mapBranches(
-            self, thisPath, caseExpr, mapBranch)
+            thisPath, caseExpr, mapBranch)
 
       if branch.isElse:
         result.add nnkElse.newTree(newStmtList(cbRes, branchBody))
@@ -48,25 +52,90 @@ proc mapBranches*(
 
 
 proc mapBranches*(
-    self: NimNode,
     obj: NObjectDecl,
-    caseExpr: proc(path: NObjectPath): NimNode,
-    mapBranch: proc(path: NObjectPath, branch: NObjectBranch): NimNode
+    caseExpr: proc(path: seq[ObjectField[NimNode]]): NimNode,
+    mapBranch: proc(path: seq[ObjectField[NimNode]], branch: NObjectBranch): NimNode
   ): NimNode =
   result = newStmtList()
   for field in items(obj.fields):
     if field.isKind:
-      result.add field.mapBranches(self, @[], caseExpr, mapBranch)
+      result.add field.mapBranches(@[field], caseExpr, mapBranch)
 
 template mapSelfBranches*(obj: NObjectDecl, expr, body: untyped): untyped =
   mapBranches(
-    ident "self", obj,
-    proc(path {.inject.}: NObjectPath): NimNode = expr,
-    proc(path {.inject.}: NObjectPath,
+    obj,
+    proc(path {.inject.}: seq[ObjectField[NimNode]]): NimNode = expr,
+    proc(path {.inject.}: seq[ObjectField[NimNode]],
          branch {.inject.}: NObjectBranch): NimNode = body
   )
 
-proc newIdent*(str: string): NimNode = newIdentNode(str)
+proc fixEmptyStmt*(node: NimNode): NimNode =
+  if isEmptyNode(node):
+    newDiscardStmt()
+
+  else:
+    node
+
+
+proc mapCase*(
+    field: NObjectField,
+    parent: seq[NObjectField],
+    caseExpr: proc(field: NObjectField, path: seq[NObjectField]): NimNode,
+    cb: proc(field: NObjectField, path: seq[NObjectField]): NimNode
+  ): NimNode =
+
+  if field.isKind:
+    result = nnkCaseStmt.newTree(caseExpr(field, parent))
+    for branch in field.branches:
+      if branch.isElse:
+        var body = nnkElse.newTree()
+        for field in branch.flds:
+          if field.isKind:
+            body.add mapCase(field, parent & field, caseExpr, cb)
+
+          else:
+            body.add mapCase(field, parent, caseExpr, cb)
+
+        result.add fixEmptyStmt(body)
+
+      else:
+        var body = newStmtList()
+        for field in branch.flds:
+          if field.isKind:
+            body.add mapCase(field, parent & field, caseExpr, cb)
+
+          else:
+            body.add mapCase(field, parent, caseExpr, cb)
+
+
+        result.add nnkOfBranch.newTree(
+          normalizeSet(branch.ofValue),
+          fixEmptyStmt(body))
+
+
+    let cbRes = cb(field, parent)
+    if cbRes != nil:
+      result = newStmtList(cbRes, result)
+
+  else:
+    result = newStmtList(cb(field, parent))
+
+proc mapCase*(
+    objectDecl: NObjectDecl,
+    caseExpr: proc(field: NObjectField, path: seq[NObjectField]): NimNode,
+    cb: proc(field: NObjectField, path: seq[NObjectField]): NimNode
+  ): NimNode =
+
+  result = newStmtList()
+  for field in objectDecl.flds:
+    result.add field.mapCase(@[field], caseExpr, cb)
+
+template mapSelfCase*(objectDecl: NObjectDecl, expr, body: untyped): untyped =
+  mapCase(
+    objectDecl,
+    proc(field {.inject.}: NObjectField, path {.inject.}: seq[NObjectField]): NimNode = expr,
+    proc(field {.inject.}: NObjectField, path {.inject.}: seq[NObjectField]): NimNode = body
+  )
 
 macro genXmlWriter*(obj: typedesc, stream, target: untyped) =
   let
@@ -97,13 +166,12 @@ macro genXmlWriter*(obj: typedesc, stream, target: untyped) =
       result = newcall("writeXml", stream, self.dotField(field))
 
   echo writeBody.toStrLit()
-# newDotExpr(self, ident field.name)
 
-  let branches = impl.mapSelfBranches(
-    tern(path.len == 0,
-         newIdent("k1"),
-         newIdent(path[^1].kindField.name))
-  ):
-    newCall("load", newIdent(path[^1].kindField.name))
+  let branches2 = impl.mapSelfCase(newIdent(field.name)):
+    var res = newStmtList()
+    for kind in path:
+      res.add newCall("print", newIdent(kind.name))
 
-  echo branches.toStrLit()
+    res.add newCall("load", newIdent(field.name))
+
+  echo branches2.toStrLit()
