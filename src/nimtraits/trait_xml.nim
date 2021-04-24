e3daf8cad45ee632288ddae5f4b2d270a724c1e6
haxscramper <haxscramper@gmail.com>
2021-04-24 23:16:10 +0300

    [CHECKPOINT]

--- src/nimtraits/trait_xml.nim
+++ src/nimtraits/trait_xml.nim
@@ -1,10 +1,74 @@
 import ../nimtraits
 import hnimast
+import hmisc/helpers
 
 import hpprint
 
 proc dotField*[N](self: N, field: ObjectField[N]): N =
   newNTree[N](nnkDotExpr, self, newNIdent[N](field.name))
+
+proc newObjectPathElem*[N](
+    field: ObjectField[N], branch: OBjectBranch[N]): ObjectPathElem[N] =
+
+  if branch.isElse:
+    NObjectPathElem(
+      isElse: true, kindField: field,
+      notOfValue: branch.notOfValue)
+
+  else:
+    NObjectPathElem(
+      isElse: false, kindField: field,
+      ofValue: normalizeSet(branch.ofValue))
+
+type
+  KindFields[N] = seq[ObjectField[N]]
+  NKindFields* = KindFields[NimNode]
+
+proc mapBranches*(
+    field: NObjectField, parent: NKindFields,
+    caseExpr: proc(kinds: NKindFields): NimNode,
+    mapBranch: proc(kinds: NKindFields, branch: NObjectBranch): NimNode
+  ): NimNode =
+
+  if field.isKind:
+    result = nnkCaseStmt.newTree(caseExpr(parent))
+    for branch in field.branches:
+      let thisPath = parent & field
+      let cbRes = mapBranch(thisPath, branch).nilToDiscard()
+
+      var branchBody = newStmtList()
+      for field in branch.flds:
+        if field.isKind:
+          branchBody.add mapBranches(
+            field, thisPath, caseExpr, mapBranch)
+
+      if branch.isElse:
+        result.add nnkElse.newTree(newStmtList(cbRes, branchBody))
+
+      else:
+        result.add nnkOfBranch.newTree(
+          normalizeSet(branch.ofValue), newStmtList(cbRes, branchBody))
+
+
+proc mapBranches*(
+    obj: NObjectDecl,
+    caseExpr: proc(kinds: NKindFields): NimNode,
+    mapBranch: proc(kinds: NKindFields, branch: NObjectBranch): NimNode
+  ): NimNode =
+  result = newStmtList()
+  for field in items(obj.fields):
+    if field.isKind:
+      result.add field.mapBranches(@[field], caseExpr, mapBranch)
+
+template mapSelfBranches*(obj: NObjectDecl, expr, body: untyped): untyped =
+  mapBranches(
+    ident "self", obj,
+    proc(path {.inject.}: NKindFields): NimNode = expr,
+    proc(path {.inject.}: NKindFields,
+         branch {.inject.}: NObjectBranch): NimNode = body
+  )
+
+proc newIdent*(str: string): NimNode = newIdentNode(str)
 
 macro genXmlWriter*(obj: typedesc, stream, target: untyped) =
   let
@@ -22,8 +86,6 @@ macro genXmlWriter*(obj: typedesc, stream, target: untyped) =
 
   echo writeKind.toStrLit()
 
-
-  var inAttrs = true
   let writeAttrs = eachCase(self, impl) do(field: TraitField) -> NimNode:
     if field.isTaggedWith("Attr"):
       result = newCall(
@@ -31,8 +93,19 @@ macro genXmlWriter*(obj: typedesc, stream, target: untyped) =
         newCall("$", self.dotField(field)))
 
   echo writeAttrs.toStrLit()
+
   let writeBody = eachCase(self, impl) do(field: TraitField) -> NimNode:
     if not field.isTaggedWith("Attr"):
       result = newcall("writeXml", stream, self.dotField(field))
 
   echo writeBody.toStrLit()
+# newDotExpr(self, ident field.name)
+
+  let branches = impl.mapSelfBranches(
+    tern(path.len == 0,
+         newIdent("k1"),
+         newIdent(path[^1].kindField.name))
+  ):
+    newCall("load", newIdent(path[^1].kindField.name))
+
+  echo branches.toStrLit()
--- tests/tNimTraits.nim
+++ tests/tNimTraits.nim
@@ -272,17 +272,30 @@ suite "`trait`":
     template Attr() {.pragma.}
 
     type
-      EqUser {.Eq, XmlIO.} = object
-        case a: bool
-          of true:
-            fld {.Attr.}: int
+      En1 = enum e1First, e1FirstCopy1, e1FirstCopy2, e1Second
+      En2 = enum e2First, e2Second
+      En3 = enum e3First, e3Second
 
-          of false:
-            fld2: float
-            field2: string
+      Obj {.Eq, XmlIO.} = object
+        case k1: En1
+          of e1First .. e1FirstCopy2:
+             case k2: En2
+               of e2First:
+                 f12first: string
+
+               of e2Second:
+                 f12second: string
+
+          of e1Second:
+            case k3: En3
+              of e3First:
+                f13first: string
+
+              of e3Second:
+                f13second: string
 
 
-    storeTraits(EqUser)
+    storeTraits(Obj)
 
-    proc writeXml(stream: Stream, target: EqUser) =
-      genXmlWriter(EqUser, stream, target)
+    proc writeXml(stream: Stream, target: Obj) =
+      genXmlWriter(Obj, stream, target)
