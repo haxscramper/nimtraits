51a1822a47980fe2e5309ecb32b2bdfa3880c7e5
haxscramper <haxscramper@gmail.com>
2021-04-28 11:44:10 +0300

    [CLEAN] Move ast builder helpers to hnimast

--- src/nimtraits/trait_xml.nim
+++ src/nimtraits/trait_xml.nim
@@ -4,293 +4,6 @@ import hmisc/helpers
 
 import hpprint
 
-proc isFinalBranch*[N](field: ObjectField[N]): bool =
-  if not field.isKind:
-    return true
-
-  else:
-    result = true
-    for branch in items(field.branches):
-      for field in branch.flds:
-        if field.isKind:
-          return false
-
-proc fixEmptyStmt*(node: NimNode): NimNode =
-  if isEmptyNode(node):
-    newDiscardStmt()
-
-  else:
-    node
-
-
-
-proc newIdent*(str: string): NimNode = newIdentNode(str)
-proc newVar*[N](name: string, varType: NType[N], default: N = nil): N =
-  newNTree[N](nnkVarSection, newNTree[N](
-    nnkIdentDefs,
-    newNIdent[N](name),
-    toNNode[N](varType),
-    (if isNil(default): newEmptyNNode[N]() else: default)
-  ))
-
-proc newDot*[N](self: N, field: ObjectField[N]): N =
-  newNTree[N](nnkDotExpr, self, newNIdent[N](field.name))
-
-proc newDot*[N](self: string, field: ObjectField[N]): N =
-  newNTree[N](nnkDotExpr, newNIdent[N](self), newNIdent[N](field.name))
-
-proc newDot*[N](self: N, name: string): N =
-  newNTree[N](nnkDotExpr, self, newNIdent[N](name))
-
-proc newSet*[N](elements: varargs[N]): N = newNTree[N](nnkCurly, elements)
-proc newDot*[N](lhs, rhs: N): N = newNTree[N](nnkCurly, lhs, rhs)
-
-proc newExprColon*[N](lhs, rhs: N): N =
-  newNTree[N](nnkExprColonExpr, lhs, rhs)
-
-proc addBranchBody*[N](main: var N, branch: ObjectBranch[N], body: N) =
-  let branchBody = fixEmptyStmt(body)
-
-  if branch.isElse:
-    main.add nnkElse.newTree(branchBody)
-
-  else:
-    main.add nnkOfBranch.newTree(
-      newSet(branch.ofValue), branchBody)
-
-
-template mapItKindFields*(branch: NObjectBranch, body: untyped): untyped =
-  var branchBody = newStmtList()
-  for field {.inject.} in branch.flds:
-    if field.isKind:
-      branchBody.add body
-
-  branchBody
-
-
-
-
-proc newObjectPathElem*[N](
-    field: ObjectField[N], branch: OBjectBranch[N]): ObjectPathElem[N] =
-
-  if branch.isElse:
-    NObjectPathElem(
-      isElse: true, kindField: field,
-      notOfValue: branch.notOfValue)
-
-  else:
-    NObjectPathElem(
-      isElse: false, kindField: field,
-      ofValue: branch.ofValue)
-
-type
-  KindFields[N] = seq[ObjectField[N]] # Using this typedef makes semcheck fail. 10/10
-  NKindFields = KindFields[NimNode]
-
-proc mapBranches*(
-    field: NObjectField,
-    parent: seq[ObjectField[NimNode]],
-    caseExpr: proc(path: seq[ObjectField[NimNode]]): NimNode,
-    mapBranch: proc(path: seq[ObjectField[NimNode]],
-                    branch: NObjectBranch): NimNode
-  ): NimNode =
-
-  if field.isKind:
-    result = nnkCaseStmt.newTree(caseExpr(parent))
-    for branch in field.branches:
-      let thisPath = parent & field # newObjectPathElem(field, branch)
-      result.addBranchBody(
-        branch,
-        newStmtList(
-          mapBranch(thisPath, branch).fixEmptyStmt(),
-          branch.mapItKindFields(field.mapBranches(
-            thisPath, caseExpr, mapBranch))))
-
-
-proc mapBranches*(
-    obj: NObjectDecl,
-    caseExpr: proc(path: seq[ObjectField[NimNode]]): NimNode,
-    mapBranch: proc(path: seq[ObjectField[NimNode]],
-                    branch: NObjectBranch): NimNode
-  ): NimNode =
-  result = newStmtList()
-  for field in items(obj.flds):
-    if field.isKind:
-      result.add field.mapBranches(@[field], caseExpr, mapBranch)
-
-template mapItBranches*(obj: NObjectDecl, expr, body: untyped): untyped =
-  mapBranches(
-    obj,
-    proc(path {.inject.}: seq[ObjectField[NimNode]]): NimNode = expr,
-    proc(path {.inject.}: seq[ObjectField[NimNode]],
-         branch {.inject.}: NObjectBranch): NimNode = body
-  )
-
-
-proc mapCase*(
-    field: NObjectField,
-    parent: seq[NObjectField],
-    caseExpr: proc(field: NObjectField, path: seq[NObjectField]): NimNode,
-    cb: proc(field: NObjectField, path: seq[NObjectField]): NimNode
-  ): NimNode =
-
-  if field.isKind:
-    result = nnkCaseStmt.newTree(caseExpr(field, parent))
-    for branch in field.branches:
-      if branch.isElse:
-        var body = nnkElse.newTree()
-        for field in branch.flds:
-          if field.isKind:
-            body.add mapCase(field, parent & field, caseExpr, cb)
-
-          else:
-            body.add mapCase(field, parent, caseExpr, cb)
-
-        result.add fixEmptyStmt(body)
-
-      else:
-        var body = newStmtList()
-        for field in branch.flds:
-          if field.isKind:
-            body.add mapCase(field, parent & field, caseExpr, cb)
-
-          else:
-            body.add mapCase(field, parent, caseExpr, cb)
-
-
-        result.add nnkOfBranch.newTree(
-          newSet(branch.ofValue),
-          fixEmptyStmt(body))
-
-
-    let cbRes = cb(field, parent)
-    if cbRes != nil:
-      result = newStmtList(cbRes, result)
-
-  else:
-    result = newStmtList(cb(field, parent))
-
-proc mapCase*(
-    objectDecl: NObjectDecl,
-    caseExpr: proc(field: NObjectField, path: seq[NObjectField]): NimNode,
-    cb: proc(field: NObjectField, path: seq[NObjectField]): NimNode
-  ): NimNode =
-
-  result = newStmtList()
-  for field in objectDecl.flds:
-    result.add field.mapCase(@[field], caseExpr, cb)
-
-template mapItCase*(objectDecl: NObjectDecl, expr, body: untyped): untyped =
-  mapCase(
-    objectDecl,
-    proc(field {.inject.}: NObjectField,
-         path {.inject.}: seq[NObjectField]): NimNode = expr,
-    proc(field {.inject.}: NObjectField,
-         path {.inject.}: seq[NObjectField]): NimNode = body
-  )
-
-
-proc mapGroups*(
-    field: NObjectField,
-    parent: seq[ObjectField[NimNode]],
-    caseExpr: proc(path: seq[ObjectField[NimNode]]): NimNode,
-    mapGroup: proc(path: seq[ObjectField[NimNode]], group: seq[NObjectField]): NimNode
-  ): NimNode =
-
-  if field.isKind:
-    let thisPath = parent & field
-    result = nnkCaseStmt.newTree(caseExpr(thisPath))
-    for branch in field.branches:
-      result.addBranchBody(
-        branch,
-        newStmtList(
-          mapGroup(thisPath, branch.flds).fixEmptyStmt(),
-          mapItKindFields(branch, field.mapGroups(
-            thisPath, caseExpr, mapGroup))))
-
-  return fixEmptyStmt(result)
-
-
-proc mapGroups*(
-    obj: NObjectDecl,
-    caseExpr: proc(path: seq[ObjectField[NimNode]]): NimNode,
-    mapGroup: proc(path: seq[ObjectField[NimNode]], group: seq[NObjectField]): NimNode
-  ): NimNode =
-
-  result = newStmtList mapGroup(@[], obj.flds)
-  for fld in items(obj.flds):
-    if fld.isKind:
-      result.add fld.mapGroups(@[], caseExpr, mapGroup)
-
-  return fixEmptyStmt(result)
-
-template mapItGroups*(objectDecl: NObjectDecl, expr, body: untyped): untyped =
-  mapGroups(
-    objectDecl,
-    proc(path {.inject.}: seq[NObjectField]): NimNode = expr,
-
-    proc(path {.inject.}: seq[NObjectField],
-         group {.inject.}: seq[NObjectField]): NimNode = body
-  )
-
-
-proc mapPath*(
-    fld: NObjectField,
-    parentField: seq[NObjectField],
-    parentPath: NObjectPath,
-    caseExpr: proc(path: seq[NObjectField]): NimNode,
-    cb: proc(path: NObjectPath, fields: seq[NObjectField]): NimNode
-  ): NimNode =
-
-  if fld.isKind:
-    result = nnkCaseStmt.newTree(caseExpr(parentField))
-    for branch in fld.branches:
-      let thisPath = parentPath & newObjectPathElem(fld, branch)
-      result.addBranchBody(
-        branch,
-        newStmtList(
-          cb(thisPath, branch.flds).fixEmptyStmt(),
-          branch.mapItKindFields(field.mapPath(
-            parentField & field, thisPath, caseExpr, cb))))
-
-proc mapPath*(
-    obj: NObjectDecl,
-    caseExpr: proc(path: seq[NObjectField]): NimNode,
-    cb: proc(path: NObjectPath, fields: seq[NObjectField]): NimNode
-  ): NimNode =
-  result = newStmtList cb(@[], obj.flds).fixEmptyStmt()
-  for fld in items(obj.flds):
-    if fld.isKind:
-      result.add fld.mapPath(@[fld], @[], caseExpr, cb)
-
-
-template mapItPath*(objectDecl: NObjectDecl, expr, body: untyped): untyped =
-  mapPath(
-    objectDecl,
-    proc(path {.inject.}: seq[NObjectField]): NimNode = expr,
-
-    proc(path {.inject.}: NObjectPath,
-         group {.inject.}: seq[NObjectField]): NimNode = body
-  )
-
-
-proc getFlatFieldsPath*[N](decl: ObjectDecl[N]):
-    seq[tuple[field: ObjectField[N], path: ObjectPath[N]]] =
-
-  proc aux(
-      parent: ObjectPath[N], field: ObjectField[N]
-    ): seq[(ObjectField[N], ObjectPath[N])] =
-
-    result.add (field, parent)
-    if field.isKind:
-      for branch in field.branches:
-        for field in branch.flds:
-          result.add aux(parent & newObjectPathElem(field, branch), field)
-
-  for field in decl.flds:
-    result.add aux(@[], field)
-
-
 macro genNewObject*(obj: typedesc) =
   let impl = getObjectStructure(obj)
 
@@ -328,7 +41,7 @@ import std/streams
 type
   XmlWriter* = object
     stream: Stream
-    indent: string
+    indentBuf: string
 
 using writer: var XmlWriter
 
@@ -338,18 +51,26 @@ proc newXmlWriter*(stream: Stream): XmlWriter =
 
 proc space*(writer) = writer.stream.write(" ")
 proc line*(writer) = writer.stream.write("\n")
-proc indent*(writer) = writer.indent.add "  "
-proc dedent*(writer) = writer.indent.setLen(max(writer.indent.len - 2, 0))
-proc writeInd*(writer) = writer.stream.write(writer.indent)
+proc indent*(writer) = writer.indentBuf.add "  "
+proc dedent*(writer) =
+  writer.indentBuf.setLen(max(writer.indentBuf.len - 2, 0))
+proc writeInd*(writer) = writer.stream.write(writer.indentBuf)
 
-proc xmlStart*(writer; elem: string) =
-  writer.writeInd(); writer.stream.write("<", elem, ">"); writer.line()
+proc xmlStart*(writer; elem: string, indent: bool = true) =
+  if indent: writer.writeInd()
+  writer.stream.write("<", elem, ">")
+  if indent: writer.line()
 
-proc xmlEnd*(writer; elem: string) =
-  writer.writeInd(); writer.stream.write("</", elem, ">"); writer.line()
+proc xmlEnd*(writer; elem: string, indent: bool = true) =
+  if indent: writer.writeInd()
+  writer.stream.write("</", elem, ">")
+  if indent: writer.line()
 
 proc xmlOpen*(writer; elem: string) = writer.writeInd(); writer.stream.write("<", elem)
 proc xmlClose*(writer) = writer.stream.write(">")
+proc xmlCloseEnd*(writer) =
+  writer.stream.write("/>")
+  writer.line()
 
 proc xmlAttribute*(writer; key, value: string) =
   writer.stream.write(key, "=\"", value, "\"")
@@ -358,21 +79,42 @@ proc toXmlString*[T](item: T): string =
   discard
 
 
-proc writeXml*(writer; value: string, tag: string) =
-  writer.xmlStart(tag)
-  writer.stream.write(value)
-  writer.xmlEnd(tag)
+proc writeXml*(writer; value: string | int | bool, tag: string) =
+  writer.writeInd()
+  writer.xmlStart(tag, false)
+  writer.stream.write($value)
+  writer.xmlEnd(tag, false)
+  writer.line()
 
 proc writeXml*[T](writer; values: seq[T], tag: string) =
   mixin writeXml
   for it in values:
-    writer.xmlStart($typeof(it))
     writer.writeXml(it, tag)
-    writer.xmlEnd($typeof(it))
+
+proc writeXml*[T](writer; opt: Option[T], tag: string) =
+  if opt.isSome():
+    writeXml(writer, opt.get(), tag)
 
 proc writeXml*[E: enum](writer; value: E, tag: string) =
   writer.xmlAttribute(tag, $value)
 
+proc writeXml*(writer; value: Slice[int], tag: string) =
+  writer.xmlOpen(tag)
+  writer.xmlAttribute("a", $value.a)
+  writer.xmlAttribute("b", $value.b)
+  writer.xmlCloseEnd()
+
+proc writeXml*[E: enum](writer; values: set[E], tag: string) =
+  writer.xmlStart(tag)
+  writer.indent()
+  writer.writeInd()
+  for item in values:
+    writer.xmlOpen("item")
+    writer.xmlAttribute("val", $item)
+    writer.xmlCloseEnd()
+
+  writer.dedent()
+  writer.xmlEnd(tag)
 
 
 macro genXmlWriter*(obj: typedesc, input, stream, tag: untyped) =
@@ -402,12 +144,12 @@ macro genXmlWriter*(obj: typedesc, input, stream, tag: untyped) =
 
   let fieldWrite = impl.mapItGroups(input.newDot(path[^1].name)):
     var res = newStmtList()
+    res.add newCall("indent", stream)
     for field in group:
       if not (field.isKind or field.isMarkedWith("Attr")) and field.isExported:
-        res.add newCall("indent", stream)
         res.add newCall(
           "writeXml", stream, input.newDot(field), newLit(field.name))
-        res.add newCall("dedent", stream)
+    res.add newCall("dedent", stream)
 
     res
 
@@ -415,4 +157,4 @@ macro genXmlWriter*(obj: typedesc, input, stream, tag: untyped) =
   result.add newCall("xmlEnd", stream, tag)
 
 
-  echo result
+  # echo result
